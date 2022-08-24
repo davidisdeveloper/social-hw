5ef896963f298094c4efb4113cf867369d2988b8
davidisdeveloper <developerdavid08@gamil.com>
2022-08-25 00:09:02 +0900

    second commit

--- src/components/Five.js
+++ src/components/Five.js
@@ -9,11 +9,13 @@ function Five() {
             <p className={styles.con}>2. 바닷속 산소 공급</p>
             <br />
             <p className={styles.con}>3. 지구의 열 순환</p>
-
+            
             <img className={styles.img} src="https://t1.daumcdn.net/cfile/tistory/9925C6365B695A0006" alt="" />
-
+            <br />
+            <p className={styles.con}>4. 태풍 피해 사례</p>
+            <br />
+            <iframe className={styles.con} width="700" height="400" src="https://www.youtube.com/embed/8qu89-4CmWU" title="대한민국을 강타한 역대 최악의 태풍들 Top 6 !!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-       
     )
 }
 
